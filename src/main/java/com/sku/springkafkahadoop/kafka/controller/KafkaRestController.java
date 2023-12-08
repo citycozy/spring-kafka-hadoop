3ffb94b573bab746b39d3d5e622fd10451467248
citycozy <citycozyboy@gmail.com>
2023-12-08 12:16:26 +0900

    feat

--- src/main/java/com/sku/springkafkahadoop/kafka/controller/KafkaRestController.java
+++ src/main/java/com/sku/springkafkahadoop/kafka/controller/KafkaRestController.java
@@ -1,19 +1,25 @@
-package com.sku.springkafkahadoop.kafka;
+package com.sku.springkafkahadoop.kafka.controller;
 
-import com.sku.springkafkahadoop.kafka.service.KafkaProducerService;
+import com.sku.springkafkahadoop.kafka.KafkaProducerService;
+import com.sku.springkafkahadoop.kafka.service.KafkaConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class KafkaRestController {
 
     private final KafkaProducerService kafkaProducerService;
+    private final KafkaConsumerService kafkaConsumerService;
 
     @Autowired
-    public KafkaRestController(KafkaProducerService kafkaProducerService) {
+    public KafkaRestController(KafkaProducerService kafkaProducerService, KafkaConsumerService kafkaConsumerService) {
         this.kafkaProducerService = kafkaProducerService;
+        this.kafkaConsumerService = kafkaConsumerService;
     }
 
     @PostMapping("/send-message")
@@ -21,4 +27,8 @@ public class KafkaRestController {
         kafkaProducerService.sendMessage(message);
     }
 
+    @GetMapping("/get-messages")
+    public List<String> getMessages() {
+        return kafkaConsumerService.getMessages();
+    }
 }
--- src/main/java/com/sku/springkafkahadoop/kafka/service/KafkaConsumerService.java
+++ src/main/java/com/sku/springkafkahadoop/kafka/service/KafkaConsumerService.java
@@ -1,2 +1,53 @@
-package com.sku.springkafkahadoop.kafka.service;public class KafkaConsumerService {
+package com.sku.springkafkahadoop.kafka.service;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class KafkaConsumerService {
+
+    private final List<String> messages = new ArrayList<>();
+    private final String hdfsOutputPath = "hdfs://localhost:9000/user/ccb/hadoop-3.3.0/output/data.txt";
+
+
+    @KafkaListener(topics = "parsed-data-topic-test", groupId = "group-id")
+    public void listen(String message) {
+        synchronized (messages) {
+            messages.add(message);
+            writeMessageToHDFS(message);
+        }
+    }
+
+    public List<String> getMessages() {
+        synchronized (messages) {
+            return new ArrayList<>(messages);
+        }
+    }
+
+    private void writeMessageToHDFS(String message) {
+        try {
+            Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", "hdfs://localhost:9000");
+
+            FileSystem fs = FileSystem.get(conf);
+            Path outputPath = new Path(hdfsOutputPath);
+
+            try (OutputStream os = fs.create(outputPath, true)) {
+                os.write(message.getBytes());
+            }
+
+            fs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
